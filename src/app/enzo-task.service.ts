6decada85ff3127a8cc77c6658fea4b7f31ed136
enzocsantos18 <enzocsantos18@gmail.com>
2022-06-14 19:25:25 -0300

    4 files modified

--- src/app/enzo-task.service.ts
+++ src/app/enzo-task.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class EnzoTaskService {
-  constructor() {}
+  constructor() {
+    let data = JSON.parse(localStorage.getItem('tasks'));
+    this.taskList = data ? data : [];
+  }
 
   private taskList: String[] = [];
 
@@ -12,12 +15,17 @@ export class EnzoTaskService {
 
   add(task: String) {
     this.taskList.push(task);
+    this.update();
   }
 
   remove(indice) {
     this.taskList.splice(indice, 1);
+    this.update();
   }
 
+  update() {
+    localStorage.setItem('tasks', JSON.stringify(this.taskList));
+  }
   list() {
     return this.taskList;
   }
